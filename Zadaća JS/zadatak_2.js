71f2726b03bc4c8a75df998bff3da68487db60d5
dino-malekoci-ramic <55707473+dino-malekoci-ramic@users.noreply.github.com>
2020-05-10 23:04:43 +0200

    Add files via upload

---
+++ Zadaća JS/zadatak_2.js
@@ -0,0 +1,5 @@
+let slika = document.getElementById("slika");
+
+function funk() {
+    slika.src = "lol.jpg";
+}
---
+++ Zadaća JS/zadatak_4.js
@@ -0,0 +1,9 @@
+function pokazi() {
+    let s = document.getElementById("slika");
+    s.style.display = "block";
+}
+
+function sakrij() {
+    let s = document.getElementById("slika");
+    s.style.display = "none";
+}
